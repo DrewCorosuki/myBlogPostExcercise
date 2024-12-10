28b4b6a4f01cdafdc09d88b1f070efe9a60e30c8
Andrew Coros <andrew_d213@yahoo.com>
2024-12-10 17:35:53 +0800

    Role Service interface and implementaion details
    - Get All Roles

--- blog.core/Service/RoleService/RoleRetrievalService.cs
+++ blog.core/Service/RoleService/RoleRetrievalService.cs
@@ -1,4 +1,8 @@
-﻿using blog.core.Interface.IService.IRoleService;
+﻿using AutoMapper;
+using blog.core.Entities;
+using blog.core.Interface.IRepository;
+using blog.core.Interface.IService.IRoleService;
+using blog.core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,34 @@ namespace blog.core.Service.RoleService
 {
     public class RoleRetrievalService : IRoleRetrievalService
     {
+        private readonly IMapper _mapper;
+        private readonly IRoleRepository _roleRepository;
+        public RoleRetrievalService(
+            IMapper mapper,
+            IRoleRepository roleRepository) 
+        {
+            _mapper = mapper;
+            _roleRepository = roleRepository;
+        }
+
+        public async Task<RoleListViewModel> GetAllRoles()
+        {
+            var model = new RoleListViewModel();
+            try
+            {
+                var roles = await _roleRepository.GetAll();
+
+                model.Data = roles
+                    .Select(n => _mapper.Map<Role, RoleViewModel>(n))
+                    .ToList();
+                model.Success = true;
+            }
+            catch(Exception e)
+            {
+                model.StatusCode = 500;
+                model.Message = e.Message;
+            }
+            return model;
+        }
     }
 }
